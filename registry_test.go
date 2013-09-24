f4b8ed02ec8124dc09737639d299c7cc6c8a6f61
Naitik Shah <n@daaku.org>
2013-09-24 13:11:30 -0700

    registry tests

---
+++ registry_test.go
@@ -0,0 +1,54 @@
+package gangliamr_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/daaku/go.ganglia/gmon"
+	"github.com/daaku/go.ganglia/gmondtest"
+	"github.com/daaku/go.gangliamr"
+	"github.com/daaku/go.metrics"
+)
+
+func TestRegistryPrefix(t *testing.T) {
+	t.Parallel()
+	h := gmondtest.NewHarness(t)
+	defer h.Stop()
+
+	const name = "gauge_simple_metric"
+	var gauge metrics.Gauge
+	gauge = &gangliamr.Gauge{
+		Name: name,
+	}
+
+	const prefix = "prefix"
+	registry := gangliamr.Registry{
+		Prefix:       prefix,
+		Client:       h.Client,
+		TickInterval: 5 * time.Millisecond,
+	}
+	registry.Register(gauge)
+
+	const v1 = 42
+	gauge.Update(v1)
+	h.ContainsMetric(&gmon.Metric{
+		Name:  prefix + "." + name,
+		Value: fmt.Sprint(v1),
+		Unit:  "value",
+		Slope: "both",
+	})
+}
+
+func TestRegistryInvalidRegister(t *testing.T) {
+	t.Parallel()
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("was expecting a panic")
+		}
+	}()
+	registry := gangliamr.Registry{
+		TickInterval: 5 * time.Millisecond,
+	}
+	registry.Register(1)
+}
