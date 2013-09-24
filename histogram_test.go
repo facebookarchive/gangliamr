56d57987dd11ed9e65db73055aea4d0b9ea70b93
Naitik Shah <n@daaku.org>
2013-09-24 13:07:30 -0700

    TestHistogramMisconfigured

--- histogram_test.go
+++ histogram_test.go
@@ -65,3 +65,17 @@ func TestHistogramSimple(t *testing.T) {
 		Slope: "both",
 	})
 }
+
+func TestHistogramMisconfigured(t *testing.T) {
+	t.Parallel()
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("was expecting a panic")
+		}
+	}()
+	hg := &gangliamr.Histogram{}
+	registry := gangliamr.Registry{
+		TickInterval: 5 * time.Millisecond,
+	}
+	registry.Register(hg)
+}
