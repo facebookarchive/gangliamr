0365271f1ca7f6f111a2054ba7cb97ac7d727ab7
Naitik Shah <n@daaku.org>
2013-10-11 14:38:47 -0700

    test for partial seconds

--- timer_test.go
+++ timer_test.go
@@ -84,3 +84,41 @@ func TestTimerSeconds(t *testing.T) {
 		Slope: "both",
 	})
 }
+
+func TestTimerSecondsPartial(t *testing.T) {
+	t.Parallel()
+	h := gmondtest.NewHarness(t)
+	defer h.Stop()
+
+	const name = "timer_seconds_partial_metric"
+	var timer metrics.Timer
+	timer = &Timer{
+		Name:       name,
+		Resolution: time.Second,
+	}
+
+	registry := testRegistry(h.Client)
+	registry.Register(timer)
+
+	const v1 = 200 * time.Millisecond
+	timer.Update(v1)
+	registry.tick()
+	registry.write()
+	h.ContainsMetric(&gmon.Metric{
+		Name:  name + ".mean",
+		Value: "0.2",
+		Unit:  "seconds",
+		Slope: "both",
+	})
+
+	const v2 = 500 * time.Millisecond
+	timer.Update(v2)
+	registry.tick()
+	registry.write()
+	h.ContainsMetric(&gmon.Metric{
+		Name:  name + ".mean",
+		Value: "0.35",
+		Unit:  "seconds",
+		Slope: "both",
+	})
+}
